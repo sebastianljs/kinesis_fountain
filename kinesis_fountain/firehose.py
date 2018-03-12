e639af347cc154b7d10ae9c6432e17e210a90023
Sebastian Lin <jersheng.lin@duke.edu>
2018-03-11 18:40:17 -0700

    added firehose

---
+++ kinesis_fountain/firehose.py
@@ -0,0 +1,11 @@
+"""
+Extends the functionalities of lambda functions
+"""
+
+from botocore.exceptions import ClientError
+from kinesis_fountain.base_resource import BaseResource
+
+class Firehose(BaseResource):
+    """
+    Extends the functionalities of lambda functions
+    """
